43f727b8ddc096a30fed2990220c0ced562a711e
Levimateusbc1 <levimateusbc2@gmail.com>
2023-09-17 10:59:01 -0300

    header buttons

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import { Slides } from "./Slides";
 import Foter from "./Footer";
 import Card from "./Card";
-import NavBar from "./NavBar/header";
+import NavBar from "./NavBar";
+import { LogoSeteBit } from "./Logo";
 
-export { Slides, Foter, Card, NavBar };
+export { Slides, Foter, Card, NavBar, LogoSeteBit };
